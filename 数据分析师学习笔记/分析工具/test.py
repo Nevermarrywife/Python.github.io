56bb25f8e0bc2262346fa37a9f16efe66a9717e0
Nevermarrywife <sxauyx@163.com>
2021-08-25 20:58:13 +0800

    Revert "升级智能硬件筛选"
    This reverts commit 9c56f437b83be22f30d16ecac26fd87e3b9177f4.

--- 数据分析师学习笔记/分析工具/test.py
+++ 数据分析师学习笔记/分析工具/test.py
@@ -7,12 +7,7 @@ channelname = '和飞信智能家'
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
-<<<<<<< HEAD
 print(channelname.find('智23能'))
-=======
-if m>0 and d<0 and c>3 and f!=0:
-    print("yes")
->>>>>>> parent of b54100d (升级筛选条件)
 =======
 if m>0 and d<0 and c>3 and f!=0:
     print("yes")
